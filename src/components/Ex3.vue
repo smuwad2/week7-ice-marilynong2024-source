10566b12d70735ed44de7ce5bbd140db856feefd
marilynong2024-source <marilyn.ong.2024@smu.edu.sg>
2025-10-21 03:48:28 +0000

    ex3

--- src/components/Ex3.vue
+++ src/components/Ex3.vue
@@ -1,9 +1,46 @@
 <script>
-    export default { 
+import axios from 'axios';
+import { errorMessages } from 'vue/compiler-sfc';
 
-       // add code here
+export default {
 
+    // add code here
+    data () {
+        return {
+            moods: ['Happy', 'Sad', 'Angry'],
+            subject: '',
+            entry: '',
+            selMood: '',
+            outputMsg: ''
+        }
+    },
+    computed: {
+        baseUrl() {
+            if (window.location.hostname == 'localhost')
+                return 'http://localhost:3000'
+            else {
+                const codespace_host = window.location.hostname.replace('5173', '3000')
+                return `https://${codespace_host}`;
+            }
+        }
+    },
+    methods: {
+        addPost(){
+            axios.get(`${this.baseUrl}/addPost`, {
+                params: {
+                    'subject': this.subject,
+                    'entry': this.entry,
+                    'mood': this.selMood,
+                }
+            }).then(response => {
+                this.outputMsg = response.data.message
+            }).catch(error => {
+                console.log(errorMessages)
+            })
+        }
     }
+
+}
 </script>
 
 <template>
@@ -19,14 +56,19 @@
 
         Mood:
         <!-- TODO: Build a dropdown list here for selecting the mood -->
+        <select v-model="selMood">
+            <option v-for="mood in moods">
+                {{ mood }}
+            </option>
+        </select>
 
         <br>
 
         <br>
-        <button>Submit New Post</button>
+        <button @click="addPost">Submit New Post</button>
+        {{  outputMsg }}
 
-        <hr> Click  <a><router-link to="/ViewPosts/">here</router-link></a>  to return to Main Page
-       
+        <hr> Click <a><router-link to="/ViewPosts/">here</router-link></a> to return to Main Page
+
     </div>
 </template>
-
