dc7b5fcf0cd0e83d13ba2aeb8e32d405075b93c1
marilynong2024-source <marilyn.ong.2024@smu.edu.sg>
2025-10-21 03:48:21 +0000

    ex2

--- src/components/Ex2.vue
+++ src/components/Ex2.vue
@@ -3,9 +3,21 @@
         // Add code in appropriate places
         data() {
             return {
-                items: ['keyboard', 'mouse', 'iPhone', 'macbook', 'adapter']
+                items: ['keyboard', 'mouse', 'iPhone', 'macbook', 'adapter'],
+                newItem: '',
             }
             
+        },
+        methods: {
+            addItems() {
+                if (this.newItem.trim()) {
+                    this.items.push(this.newItem.trim());
+                    this.newItem = ''; // Clear input
+                }
+            },
+            deleteItem(idx) {
+                this.items.splice(idx, 1); // Remove item by index
+            }
         }
     }
 </script>
@@ -14,5 +26,13 @@
 
     <h2>Shopping Cart</h2>
     <!-- TODO: Add Code Here -->
+    <ul>
+        <li v-for="(item, index) in items" :key="index">{{ item }}
+            <button class="delete-btn" @click="deleteItem(index)">Delete</button>
+        </li>
+    </ul>
+    
+    <input v-model="newItem" type="text" placeholder="Enter item name">
+    <button type="button" @click="addItems">add</button>
    
 </template>
